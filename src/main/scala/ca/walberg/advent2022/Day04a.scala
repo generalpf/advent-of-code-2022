8cb7855d965bdc87d1beb5d5d3c170ce970da8cc
Ryan Walberg <generalpf@gmail.com>
2022-12-04 13:36:17 -0500

    finished day 4

---
+++ src/main/scala/ca/walberg/advent2022/Day04a.scala
@@ -0,0 +1,17 @@
+package ca.walberg.advent2022
+
+import scala.io.Source
+
+@main def Day04a(args: String*): Unit = {
+  val source = Source.fromFile("data/day4.txt")
+  val regex = """(\d+)-(\d+),(\d+)-(\d+)""".r
+  val score: Int = source.getLines().foldRight(0)(
+    (line: String, acc: Int) =>
+      acc + (regex.findFirstMatchIn(line) match
+        case Some(m) =>
+          if ((m.group(1).toInt <= m.group(3).toInt && m.group(2).toInt >= m.group(4).toInt) || (m.group(3).toInt <= m.group(1).toInt && m.group(4).toInt >= m.group(2).toInt)) 1 else 0
+      )
+  )
+  println(score)
+  source.close()
+}

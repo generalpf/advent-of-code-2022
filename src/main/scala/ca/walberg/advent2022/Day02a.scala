21bd84baec940cfbc9340b8c56a507ed2edfff25
Ryan Walberg <generalpf@gmail.com>
2022-12-04 12:51:46 -0500

    finished day 2

---
+++ src/main/scala/ca/walberg/advent2022/Day02a.scala
@@ -0,0 +1,22 @@
+package ca.walberg.advent2022
+
+import java.io.{BufferedReader, File, FileReader}
+import scala.io.Source
+@main def Day02a(args: String*): Unit = {
+    val source = Source.fromFile("data/day2.txt")
+    val score: Int = source.getLines().foldRight(0)(
+      (line: String, acc: Int) =>
+        acc + (line match
+          case "A X" => 3 + 1
+          case "A Y" => 6 + 2
+          case "A Z" => 0 + 3
+          case "B X" => 0 + 1
+          case "B Y" => 3 + 2
+          case "B Z" => 6 + 3
+          case "C X" => 6 + 1
+          case "C Y" => 0 + 2
+          case "C Z" => 3 + 3
+    ))
+    println(score)
+    source.close()
+}

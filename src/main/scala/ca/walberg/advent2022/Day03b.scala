4301c2103a6b160f1c0d5fe8bbe04564ba4c0cf0
Ryan Walberg <generalpf@gmail.com>
2022-12-04 13:15:27 -0500

    finished day 3

---
+++ src/main/scala/ca/walberg/advent2022/Day03b.scala
@@ -0,0 +1,18 @@
+package ca.walberg.advent2022
+
+import scala.io.Source
+
+@main def Day03b(args: String*): Unit = {
+    val source = Source.fromFile("data/day3.txt")
+    val score: Int = source.getLines().grouped(3).toList.foldRight(0)(
+      (group: Seq[String], acc: Int) =>
+        val intersection = group(0).toSet.intersect(group(1).toSet).intersect(group(2).toSet)
+        val priority = if (intersection.head.isUpper)
+          intersection.head - 64 + 26
+        else
+          intersection.head - 96
+        acc + priority
+    )
+    println(score)
+    source.close()
+}

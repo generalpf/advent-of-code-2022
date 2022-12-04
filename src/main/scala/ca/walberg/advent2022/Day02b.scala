38b2700505db9dd66c8363e444b45fc30d1eeb5b
Ryan Walberg <generalpf@gmail.com>
2022-12-04 13:36:51 -0500

    more cleanup

--- src/main/scala/ca/walberg/advent2022/Day02b.scala
+++ src/main/scala/ca/walberg/advent2022/Day02b.scala
@@ -3,20 +3,20 @@ package ca.walberg.advent2022
 import scala.io.Source
 
 @main def Day02b(args: String*): Unit = {
-    val source = Source.fromFile("data/day2.txt")
-    val score: Int = source.getLines().foldRight(0)(
-      (line: String, acc: Int) =>
-        acc + (line match
-          case "A X" => 0 + 3
-          case "A Y" => 3 + 1
-          case "A Z" => 6 + 2
-          case "B X" => 0 + 1
-          case "B Y" => 3 + 2
-          case "B Z" => 6 + 3
-          case "C X" => 0 + 2
-          case "C Y" => 3 + 3
-          case "C Z" => 6 + 1
-    ))
-    println(score)
-    source.close()
+  val source = Source.fromFile("data/day2.txt")
+  val score: Int = source.getLines().foldRight(0)(
+    (line: String, acc: Int) =>
+      acc + (line match
+        case "A X" => 0 + 3
+        case "A Y" => 3 + 1
+        case "A Z" => 6 + 2
+        case "B X" => 0 + 1
+        case "B Y" => 3 + 2
+        case "B Z" => 6 + 3
+        case "C X" => 0 + 2
+        case "C Y" => 3 + 3
+        case "C Z" => 6 + 1
+  ))
+  println(score)
+  source.close()
 }
--- src/main/scala/ca/walberg/advent2022/Day03a.scala
+++ src/main/scala/ca/walberg/advent2022/Day03a.scala
@@ -3,18 +3,18 @@ package ca.walberg.advent2022
 import scala.io.Source
 
 @main def Day03a(args: String*): Unit = {
-    val source = Source.fromFile("data/day3.txt")
-    val score: Int = source.getLines().foldRight(0)(
-      (line: String, acc: Int) =>
-        val left = line.toCharArray.take(line.length / 2).toSet
-        val right = line.toCharArray.takeRight(line.length / 2).toSet
-        val intersection = left.intersect(right)
-        val priority = if (intersection.head.isUpper)
-          intersection.head - 64 + 26
-        else
-          intersection.head - 96
-        acc + priority
-    )
-    println(score)
-    source.close()
+  val source = Source.fromFile("data/day3.txt")
+  val score: Int = source.getLines().foldRight(0)(
+    (line: String, acc: Int) =>
+      val left = line.toCharArray.take(line.length / 2).toSet
+      val right = line.toCharArray.takeRight(line.length / 2).toSet
+      val intersection = left.intersect(right)
+      val priority = if (intersection.head.isUpper)
+        intersection.head - 64 + 26
+      else
+        intersection.head - 96
+      acc + priority
+  )
+  println(score)
+  source.close()
 }

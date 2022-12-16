43c9454bceb852a0b04965dd9481096db7912a55
Ryan Walberg <generalpf@gmail.com>
2022-12-16 11:31:26 -0500

    finished day 15

---
+++ src/main/scala/ca/walberg/advent2022/Day15a.scala
@@ -0,0 +1,35 @@
+package ca.walberg.advent2022
+
+import scala.:+
+import scala.io.Source
+
+@main def Day15a(args: String*): Unit = {
+  def manhattanDistance(fromX: Int, fromY: Int, toX: Int, toY: Int): Int = Math.abs(toX - fromX) + Math.abs(toY - fromY)
+
+  val source = Source.fromFile("data/day15.txt")
+  val regex = """Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)""".r
+  val sensorsAndBeacons: Seq[((Int, Int), (Int, Int))] = source.getLines().foldLeft(Seq())(
+    (acc: Seq[((Int, Int), (Int, Int))], line: String) =>
+      regex.findFirstMatchIn(line) match
+        case Some(m) =>
+          acc :+ ((m.group(1).toInt, m.group(2).toInt), (m.group(3).toInt, m.group(4).toInt))
+  )
+  val horizontalBounds = sensorsAndBeacons.map(_._1._1) ++ sensorsAndBeacons.map(_._2._1)
+  val leftX = horizontalBounds.min
+  val rightX = horizontalBounds.max
+  val greatestMD = sensorsAndBeacons
+    .map((sensor, beacon) => manhattanDistance(sensor._1, sensor._2, beacon._1, beacon._2))
+    .max
+
+  val y = 2000000
+  val noBeaconHere = ((leftX - greatestMD) until (rightX + greatestMD + 1)).count(x =>
+    // are there any sensors that are further from their beacon than they are to this point?
+    sensorsAndBeacons.indexWhere((sensor, beacon) =>
+      // is the distance from sensor -> beacon greater than or equal to the distance from sensor -> (x, y)?
+      !(beacon._1 == x && beacon._2 == y) && manhattanDistance(sensor._1, sensor._2, beacon._1, beacon._2) >= manhattanDistance(sensor._1, sensor._2, x, y)
+    ) != -1
+  )
+
+  println(noBeaconHere)
+  source.close()
+}
---
+++ src/main/scala/ca/walberg/advent2022/Day15b.scala
@@ -0,0 +1,55 @@
+package ca.walberg.advent2022
+
+import java.util.concurrent.Executors
+import scala.:+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
+import scala.io.Source
+
+@main def Day15b(args: String*): Unit = {
+  def manhattanDistance(fromX: Int, fromY: Int, toX: Int, toY: Int): Int = Math.abs(toX - fromX) + Math.abs(toY - fromY)
+
+  val source = Source.fromFile("data/day15.txt")
+  val regex = """Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)""".r
+  val sensorsAndBeacons: Seq[((Int, Int), (Int, Int))] = source.getLines().foldLeft(Seq())(
+    (acc: Seq[((Int, Int), (Int, Int))], line: String) =>
+      regex.findFirstMatchIn(line) match
+        case Some(m) =>
+          acc :+ ((m.group(1).toInt, m.group(2).toInt), (m.group(3).toInt, m.group(4).toInt))
+  )
+
+  implicit val context: ExecutionContextExecutor = ExecutionContext.fromExecutor(Executors.newWorkStealingPool())
+
+  val dimension = 4000000
+  val threads = Runtime.getRuntime.availableProcessors
+  val slice = dimension / threads
+
+  val futures = (0 until threads).map(i =>
+    Future {
+      (i * slice until (i + 1) * slice + 1).indexWhere(y =>
+        var x = 0
+        var found = false
+        while (!found && x <= dimension) {
+          // find first sensor that we're within matching beacon range of.
+          val inRangeOf = sensorsAndBeacons.indexWhere((sensor, beacon) =>
+            // is the distance from sensor -> beacon greater than or equal to the distance from sensor -> (x, y)?
+            manhattanDistance(sensor._1, sensor._2, beacon._1, beacon._2) >= manhattanDistance(sensor._1, sensor._2, x, y)
+          )
+          if (inRangeOf == -1) {
+            println(s"($x, $y) => ${BigInt(x) * 4000000 + y}")
+            found = true
+          } else {
+            val sensorAndBeacon = sensorsAndBeacons(inRangeOf)
+            val distanceFromSensorToBeacon = manhattanDistance(sensorAndBeacon._1._1, sensorAndBeacon._1._2, sensorAndBeacon._2._1, sensorAndBeacon._2._2)
+            x = sensorAndBeacon._1._1 + (distanceFromSensorToBeacon - Math.abs(sensorAndBeacon._1._2 - y)) + 1
+          }
+        }
+        found
+      )
+    }
+  )
+
+  futures.foreach(f => Await.ready(f, Duration.Inf))
+
+  source.close()
+}
